30d677f65232f433a76bc6df4a3dd6f88f31ac25
jensung <jensunngggg@gmail.com>
2019-05-14 18:07:09 -0400

    Assignment Basic Node App: Completed

---
+++ main.js
@@ -0,0 +1,26 @@
+const greetings = require("./greeting.js");
+const farewells = require("./farewell.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+
+// "Hallo"
+greetings.greetingIcelandic();
+
+// "Good-bye"
+farewells.farewellEnglish();
+
+// "Sayōnara"
+farewells.farewellJapanese();
+
+// "Adiós"
+farewells.farewellSpanish();
+
+// "BLess"
+farewells.farewellIcelandic();
